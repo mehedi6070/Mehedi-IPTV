81a6cd1bc058244707f6d267b6e896306d40d412
Mehedi Net <mehediofficial60@gmail.com>
2026-09-26 15:18:15 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,18 @@ android {
         versionCode = 3
         versionName = "3.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
+
+
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
